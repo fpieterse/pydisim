faba7d4c25a88db604a5bfeba8090e20f3a8c27a
Francois Pieterse <francois185@gmail.com>
2018-09-15 09:38:50 +1000

    Change how PIDRecorder works.
    PIDRecorder used to contain two recorders. Changed it so that it is easier to
    save a Recorder to file (now PIDRecorder is a recorder).

--- pydisim/tools.py
+++ pydisim/tools.py
@@ -17,10 +17,11 @@ class Recorder:
             List of nice names of variables.
         '''
 
+        if type(names) != list:
+            raise ValueError('Expected list of variable names')
+
         self.names = names
-        self.data = []
-        for n in names:
-            self.data.append([])
+        self.clear()
 
 
     def record(self,*args):
@@ -60,7 +61,27 @@ class Recorder:
                 ax.plot(timestamps,self.data[i],label=self.names[i])
         ax.legend()
 
-class PIDRecorder():
+    def clear(self,keep=None):
+        '''
+        Clear the recorded history
+
+        Arguments:
+        ----------
+        keep : int
+            Number of samples to keep. If None the whole thing is cleared
+        '''
+        if keep is not None and (keep > 0):
+            for (i,d) in enumerate(self.data):
+                # this does not raise OutOfRangeExceptionata
+                self.data[i] = d[-keep:] 
+        else:
+            self.data = []
+            for i in range(len(self.names)):
+                self.data.append([])
+
+
+
+class PIDRecorder(Recorder):
     '''
     A special type of recorder used to record a PID controller. It doesn't need
     variable names when instantiated (they are always SP,PV,OP). It also makes
@@ -74,13 +95,13 @@ class PIDRecorder():
         tagname : string
             The tagname of the PID controller
         '''
-        self.sppvRec = Recorder(['SP','PV'])
-        self.opRec = Recorder(['OP'])
+        super().__init__([tagname + '.sp',
+                          tagname + '.pv',
+                          tagname + '.op'])
         self.tagname = tagname
 
     def record(self,sp,pv,op):
-        self.sppvRec.record(sp,pv)
-        self.opRec.record(op)
+        super().record(sp,pv,op)
 
     def plot(self,ax1,ax2,timestamps=None):
         '''
@@ -102,7 +123,42 @@ class PIDRecorder():
         else:
             fig = None
 
-        self.sppvRec.plot(ax1,timestamps)
-        self.opRec.plot(ax2,timestamps)
+        if timestamps is None:
+            for (i,ax) in zip((0,1,2),(ax1,ax1,ax2)):
+                ax.plot(self.data[i],label=self.names[i])
+        else:
+            for (i,ax) in zip((0,1,2),(ax1,ax1,ax2)):
+                ax.plot(timestamps,self.data[i],label=self.names[i])
+
         ax1.set_ylabel(self.tagname)
 
+
+def save_to_file(filename,time,*recorders):
+    '''
+    Save recorder data to file.
+
+    Arguments:
+    ----------
+    filename : string
+        name of file
+    time : list
+        list of timestamps corresponding to recorder data
+    *recorders : Recorder
+        Remaining arguments are recorders to be saved to file
+    '''
+
+    with open(filename,'w') as f:
+        f.write('time')
+        for r in recorders:
+            for n in r.names:
+                f.write(',' + n)
+        f.write('\n')
+
+        for i in range(len(time)):
+            f.write(str(time[i]))
+            for r in recorders:
+                for d in r.data:
+                    f.write(',{:.6f}'.format(d[i]))
+            f.write('\n')
+
+                
--- tests/01.testTools.py
+++ tests/01.testTools.py
@@ -26,6 +26,26 @@ for i in range(9):
 assert rec.data[0][0] == 0
 assert rec.data[1][8] == 16
 
+rec.clear(3)
+assert len(rec.data) == 2
+assert len(rec.data[0]) == 3
+assert rec.data[0][2] == 8
+
+
 if doPlot:
     rec.plot(plt,t)
     plt.show()
+
+rec = Recorder(['v1','v2'])
+prec = PIDRecorder('p')
+rec.record(1,10)
+rec.record(2,20)
+prec.record(3,4,5)
+prec.record(6,7,8)
+time = [1,2]
+savefile = '01.testTools.savetest.csv'
+save_to_file(savefile,time,rec,prec)
+with open(savefile,'r') as f:
+    assert f.readline() == 'time,v1,v2,p.sp,p.pv,p.op\n'
+    assert f.readline() == '1,1.000000,10.000000,3.000000,4.000000,5.000000\n'
+    assert f.readline() == '2,2.000000,20.000000,6.000000,7.000000,8.000000\n'
