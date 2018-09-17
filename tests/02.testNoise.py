29787731a4bde372738e4ddb3ed04a051533a062
Francois Pieterse <francois185@gmail.com>
2018-09-17 16:43:25 +1000

    Add tests for noise processes

--- tests/02.testNoise.py
+++ tests/02.testNoise.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('../')
 
 import matplotlib.pyplot as plt
+import numpy
 
 import pydisim as pds
 
@@ -18,9 +19,29 @@ main.add_connection(bnp,'output',gnp,'input')
 
 rec = pds.tools.Recorder(['Brown','Gauss'])
 
+# PLot random noise. Then do it again (need to see different noise)
+# THen do it again with seed to make sure we can seed the noise with numpy
 for i in range(100):
     main.run_for(10)
     rec.record(bnp.output,gnp.output)
 
 rec.plot()
 plt.show()
+
+rec.clear()
+numpy.random.seed(0)
+for i in range(100):
+    main.run_for(10)
+    rec.record(bnp.output,gnp.output)
+
+rec.plot()
+plt.show()
+rec.clear()
+numpy.random.seed(0)
+for i in range(100):
+    main.run_for(10)
+    rec.record(bnp.output,gnp.output)
+
+rec.plot()
+plt.show()
+
